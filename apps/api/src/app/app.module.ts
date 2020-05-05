98fd9dd953c6eac8ab3f622a3877a323dcff49f8
Andrew Gilbert <andrewgilbert@uchicago.edu>
2020-05-05 11:31:59 -0500

    fix typo in path

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { join } from 'path';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'nx-app'),
+      rootPath: join(__dirname, '..', 'doc-collab'),
       exclude: ['/api*']
     })
   ],
