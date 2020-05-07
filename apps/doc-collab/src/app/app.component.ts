9892e51d9c12908e89aef3db5555ad8943a656ec
Andrew Gilbert <andrewgilbert@uchicago.edu>
2020-05-07 08:43:14 -0500

    initial routing based on presence of token; basic dashboard UI

--- apps/doc-collab/src/app/app.component.ts
+++ apps/doc-collab/src/app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'doccollab-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor(private http: HttpClient) {}
+export class AppComponent implements OnInit {
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+  ) {}
+
+  private verifyLoggedIn() {
+    console.log('in logged in');
+    const token = this.authService.getToken();
+    if (!token) {
+      this.router.navigateByUrl('login');
+    } else {
+      this.router.navigateByUrl('dashboard');
+    }
+  }
+
+  ngOnInit() {
+    this.verifyLoggedIn();
+  }
+
 }
