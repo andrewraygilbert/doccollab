f6ae59b80df77fbe0d9e37f867d2708dc26f69a5
Andrew Gilbert <andrewgilbert@uchicago.edu>
2020-06-19 10:02:17 -0500

    fix env variable for REDIS

--- dist/apps/api/main.js
+++ dist/apps/api/main.js
@@ -242,7 +242,7 @@ WsAuth = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([
 const environment = {
     production: true,
     dbURI: process.env.MONGODB_URI,
-    REDIS_URI: 'redis://h:peb1153eccdd4ad8a446810f87586a0404d17b052c61a0a716864ae65007871be@ec2-52-203-67-230.compute-1.amazonaws.com:21009',
+    REDIS_URI: process.env.REDIS_URL,
     JWT_SECRET: process.env.JWT_SECRET,
     API_BASE_URL: 'https://doccollab.herokuapp.com/api/',
 };
